782212f2842fc8b1bc2abe4dee1bffacbea0dd5b
Contraboi <69647487+Contraboi@users.noreply.github.com>
2020-09-29 15:23:37 +0200

    Jebem ti boga

--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,17 +15,25 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="mainLayout.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
+    <!-- StAyles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
-
-
-<p id="pegla"> test </p>
-
-
-
+    <header>
+        <img class="logo" src="images/logo1.jpg" alt="logo">
+        <nav>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Equipment</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+        <a class="cta" href="#"><button>Contact</button></a>
+    </header>
 </body>
+
+</html>
