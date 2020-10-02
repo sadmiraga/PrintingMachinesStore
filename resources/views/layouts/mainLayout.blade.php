8a439d45a857333d9e5f280081946b205fc2290f
Contraboi <69647487+Contraboi@users.noreply.github.com>
2020-10-02 18:23:02 +0200

    Navbar v3

--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -2,8 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -24,9 +25,12 @@
 </head>
 
 <body>
+    <!-- Navbar -->
     <header id="navbarID">
         <img class="logo" id="logoID" src="images/logoBlack.png" alt="logo">
         <nav>
+            <label for="toggle">&#9776;</label>
+            <input type="checkbox" id="toggle" />
             <ul class="nav-links">
                 <li class="navbar-li"><a id="home" href="#">Home</a></li>
                 <li class="navbar-li"><a id="equipment" href="#">Equipment</a></li>
@@ -34,50 +38,203 @@
             </ul>
         </nav>
         <a href="#"><button id="navbar-buttonID">Contact</button></a>
+        <div class="dropdown">
+            <p class="language">English(EN)</p>
+            <ul class="dropdown-content">
+                <li><a href="#">Slovenian(SL)</a></li>
+                <li><a href="#">German(GE)</a></li>
+                <li><a href="#">French(FR)</a></li>
+        </div>
+        </div>
+
+        <!--Navbar script -->
+        <script>
+        window.onscroll = function() {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+                //small navbar
+                var counter = 0;
+                document.getElementById("counter").innerHTML = counter;
+                document.getElementById("navbarID").style.backgroundColor = "#6AABC1";
+                document.getElementById("navbarID").style.fontSize = "20px";
+                document.getElementById("navbarID").style.padding = "2px 5%";
+                document.getElementById("logoID").style.height = "80px";
+                document.getElementById("logoID").src = "images/logoWhite.png";
+                document.getElementById("home").style.color = "white";
+                document.getElementById("home").style.marginTop = "20px";
+                document.getElementById("equipment").style.color = "white";
+                document.getElementById("about").style.color = "white";
+                document.getElementById("navbar-buttonID").style.backgroundColor = "white";
+                document.getElementById("navbar-buttonID").style.color = "#0088a9";
+
+                if (counter == 0) {
+                    document.getElementById("counter").style.color = "white";
+                    //Home animation small
+                    document.getElementById("home").onmouseover = function() {
+                        mouseOverSmallHome()
+                    };
+                    document.getElementById("home").onmouseout = function() {
+                        mouseOutSmallHome()
+                    };
+
+                    function mouseOverSmallHome() {
+                        document.getElementById("home").style.color = "#105561";
+
+                    }
+
+                    function mouseOutSmallHome() {
+                        document.getElementById("home").style.color = "white";
+                    }
+                    //Equipment animation small
+                    document.getElementById("equipment").onmouseover = function() {
+                        mouseOverSmallEquipment()
+                    };
+                    document.getElementById("equipment").onmouseout = function() {
+                        mouseOutSmallEquipment()
+                    };
+
+                    function mouseOverSmallEquipment() {
+                        document.getElementById("equipment").style.color = "#105561";
+                    }
+
+                    function mouseOutSmallEquipment() {
+                        document.getElementById("equipment").style.color = "white";
+                    }
+
+                    //About animation small
+                    document.getElementById("about").onmouseover = function() {
+                        mouseOverSmallAbout()
+                    };
+                    document.getElementById("about").onmouseout = function() {
+                        mouseOutSmallAbout()
+                    };
+
+                    function mouseOverSmallAbout() {
+                        document.getElementById("about").style.color = "#105561";
+                    }
+
+                    function mouseOutSmallAbout() {
+                        document.getElementById("about").style.color = "white";
+                    }
+
+                    //Contact-button animation small
+                    document.getElementById("navbar-buttonID").onmouseover = function() {
+                        mouseOverSmallContactBtn()
+                    };
+                    document.getElementById("navbar-buttonID").onmouseout = function() {
+                        mouseOutSmallContactBtn()
+                    };
+
+                    function mouseOverSmallContactBtn() {
+                        document.getElementById("navbar-buttonID").style.color = "white";
+                        document.getElementById("navbar-buttonID").style.backgroundColor = "#0088a9";
+                    }
+
+                    function mouseOutSmallContactBtn() {
+                        document.getElementById("navbar-buttonID").style.color = "#0088a9";
+                        document.getElementById("navbar-buttonID").style.backgroundColor = "white";
+                    }
+                }
+
+            } else {
+                counter = 1;
+                document.getElementById("counter").innerHTML = counter;
+                document.getElementById("navbarID").style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+                document.getElementById("navbarID").style.fontSize = "20px";
+                document.getElementById("navbarID").style.padding = "2px 5%";
+                document.getElementById("logoID").style.height = "120px";
+                document.getElementById("logoID").src = "images/logoBlack.png";
+                document.getElementById("home").style.color = "#0088a9";
+                document.getElementById("equipment").style.color = "#0088a9";
+                document.getElementById("about").style.color = "#0088a9";
+                document.getElementById("navbar-buttonID").style.color = "white";
+                document.getElementById("navbar-buttonID").style.backgroundColor = "#0088a9";
+                if (counter == 1) {
+                    //Home animation big
+                    document.getElementById("home").onmouseover = function() {
+                        mouseOverBigHome()
+                    };
+                    document.getElementById("home").onmouseout = function() {
+                        mouseOutBigHome()
+                    };
+
+                    function mouseOverBigHome() {
+                        document.getElementById("home").style.color = "black";
+                    }
+
+                    function mouseOutBigHome() {
+                        document.getElementById("home").style.color = "#0088a9";
+                    }
+
+                    //Equipment animation big
+                    document.getElementById("equipment").onmouseover = function() {
+                        mouseOverBigEquipment()
+                    };
+                    document.getElementById("equipment").onmouseout = function() {
+                        mouseOutBigEquipment()
+                    };
+
+                    function mouseOverBigEquipment() {
+                        document.getElementById("equipment").style.color = "black";
+                    }
+
+                    function mouseOutBigEquipment() {
+                        document.getElementById("equipment").style.color = "#0088a9";
+                    }
+
+                    //About animation big
+                    document.getElementById("about").onmouseover = function() {
+                        mouseOverBigAbout()
+                    };
+                    document.getElementById("about").onmouseout = function() {
+                        mouseOutBigAbout()
+                    };
+
+                    function mouseOverBigAbout() {
+                        document.getElementById("about").style.color = "black";
+                    }
+
+                    function mouseOutBigAbout() {
+                        document.getElementById("about").style.color = "#0088a9";
+                    }
+
+                    //Contact Button animation big
+                    document.getElementById("navbar-buttonID").onmouseover = function() {
+                        mouseOverBigContactBtn()
+                    };
+                    document.getElementById("navbar-buttonID").onmouseout = function() {
+                        mouseOutBigContactBtn()
+                    };
+
+                    function mouseOverBigContactBtn() {
+                        document.getElementById("navbar-buttonID").style.color = "white";
+                        document.getElementById("navbar-buttonID").style.backgroundColor = "#105561";
+                    }
+
+                    function mouseOutBigContactBtn() {
+                        document.getElementById("navbar-buttonID").style.color = "white";
+                        document.getElementById("navbar-buttonID").style.backgroundColor = "#0088a9";
+                    }
+                }
+            }
+        }
+        </script>
+
     </header>
 
-
-    <script>
-    window.onscroll = function() {
-        scrollFunction()
-    };
-
-    function scrollFunction() {
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            document.getElementById("navbarID").style.backgroundColor = "#6AABC1";
-            document.getElementById("navbarID").style.fontSize = "20px";
-            document.getElementById("navbarID").style.padding = "2px 5%";
-            document.getElementById("logoID").style.height = "80px";
-            document.getElementById("logoID").src = "images/logoWhite.png";
-            document.getElementById("home").style.color = "white";
-            document.getElementById("home").style.marginTop = "20px";
-            document.getElementById("equipment").style.color = "white";
-            document.getElementById("about").style.color = "white";
-            document.getElementById("navbar-buttonID").style.backgroundColor = "white";
-            document.getElementById("navbar-buttonID").style.color = "#0088a9";
-        } else {
-            document.getElementById("navbarID").style.backgroundColor = "rgba(255, 255, 255, 0.8)";
-            document.getElementById("navbarID").style.fontSize = "20px";
-            document.getElementById("navbarID").style.padding = "2px 5%";
-            document.getElementById("logoID").style.height = "120px";
-            document.getElementById("logoID").src = "images/logoBlack.png";
-            document.getElementById("home").style.color = "#0088a9";
-            document.getElementById("equipment").style.color = "#0088a9";
-            document.getElementById("about").style.color = "#0088a9";
-            document.getElementById("navbar-buttonID").style.color = "white";
-            document.getElementById("navbar-buttonID").style.backgroundColor = "#0088a9";
-
-        }
-    }
-    </script>
-
+    <!-- Content -->
     <div class="hero">
         <img class="heroimg" src="images/heroTest.png">
     </div>
     <div class="content">
-        content
+        <p style="color: red" id="counter">counter</p>
+
     </div>
 
+    <!-- Footer -->
     <footer class="footer">
         <div class="contact">
             <ul class="footer-ul">
@@ -100,9 +257,6 @@
             <img class="footer-linkedin" src="/images/linkedin-white.png"
                 onmouseover="this.src='/images/linkedin-yellow.png';"
                 onmouseout="this.src='/images/linkedin-white.png';" />
-        </div>
-
-        <div class="footer-icons">
         </div>
     </footer>
 
